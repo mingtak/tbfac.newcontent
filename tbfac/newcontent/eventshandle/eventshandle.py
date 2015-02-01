9724a151a9d7c0cddab0a720388e872223546922
andy fang <andy@mingtak.com.tw>
2015-02-01 08:19:10 +0000

    modify, art&life eventhandle OK.

--- tbfac/newcontent/eventshandle/eventshandle.py
+++ tbfac/newcontent/eventshandle/eventshandle.py
@@ -1,6 +1,12 @@
 from plone import api
 from Products.CMFPlone.utils import safe_unicode
-from AccessControl.SecurityManagement import getSecurityManager
+
+
+def setFolderState(folder, userId, rolesList, allowedTypes):
+    folder.setConstrainTypesMode(1)
+    folder.setLocallyAllowedTypes(allowedTypes)
+    folder.manage_setLocalRoles(userId, rolesList)
+    folder.reindexObjectSecurity()
 
 
 def checkRoles(event):
@@ -13,34 +19,34 @@ def checkRoles(event):
     user = api.user.get(userid=userId)
     userRoles = user.getRoles()
     username = safe_unicode(user.getProperty('fullname'))
+    folderPath = "/art_and_life/%s" % userId
+    folder = api.content.get(path=folderPath)
 
     if 'ArtAndLife' not in userRoles:
         user.setMemberProperties({'wysiwyg_editor': 'CKeditor'})
+        if folder is None:
+            return
         with api.env.adopt_user(username="root"):
-            folderPath = "/taishin/art_and_life/%s" % userId
-            folder = api.content.get(path=folderPath)
-            folder.setConstrainTypesMode(1)
-            folder.setLocallyAllowedTypes(('Image', 'tbfac.newcontent.artandlife'))
-            folder.manage_setLocalRoles(userId, ['Member'])
-            folder.reindexObjectSecurity()
+            setFolderState(folder,
+                           userId,
+                           ['Member'],
+                           ('Image', 'tbfac.newcontent.artandlife'))
         return
 
     user.setMemberProperties({'wysiwyg_editor': 'TinyMCE'})
-    folderPath = "/taishin/art_and_life/%s" % userId
     catalog = portal.portal_catalog
-    if len(catalog(path=folderPath)) > 0:
+    if folder is not None:
         with api.env.adopt_user(username="root"):
-            folder = api.content.get(path=folderPath)
-            folder.setConstrainTypesMode(1)
-            folder.setLocallyAllowedTypes(('Image', 'tbfac.newcontent.artandlife'))
-            folder.manage_setLocalRoles(userId, ['Contributor', 'Editor', 'Reviewer', 'Member'])
-            folder.reindexObjectSecurity()
+            setFolderState(folder,
+                           userId,
+                           ['Contributor', 'Editor', 'Reviewer', 'Member'],
+                           ('Image', 'tbfac.newcontent.artandlife'))
         return
 
     with api.env.adopt_user(username="root"):
         folder = api.content.create(container=container, type="Folder", id=userId, title=username)
         api.content.transition(obj=folder, transition='publish')
-        folder.setConstrainTypesMode(1)
-        folder.setLocallyAllowedTypes(('Image', 'tbfac.newcontent.artandlife'))
-        folder.manage_setLocalRoles(userId, ['Contributor', 'Editor', 'Reviewer', 'Member'])
-        folder.reindexObjectSecurity()
+        setFolderState(folder,
+                       userId,
+                       ['Contributor', 'Editor', 'Reviewer', 'Member'],
+                       ('Image', 'tbfac.newcontent.artandlife'))
