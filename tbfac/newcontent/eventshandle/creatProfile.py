029d2d4d8c2f28aa5355aea6c6c8ff47c0429df2
TsungWei Hu <marr.tw@gmail.com>
2015-12-03 12:11:15 +0800

    modified creatProfile.py

--- tbfac/newcontent/eventshandle/creatProfile.py
+++ tbfac/newcontent/eventshandle/creatProfile.py
@@ -3,10 +3,39 @@ from plone import api
 from Products.CMFPlone.utils import safe_unicode
 
 from Products.PlonePAS.events import UserInitialLoginInEvent
+from Products.PlonePAS.events import UserLoggedInEvent
+
+
+@grok.subscribe(UserLoggedInEvent)
+def checkProfile(event):
+    portal = api.portal.get()
+    catalog = portal.portal_catalog
+    currentUser = event.object
+#    import pdb; pdb.set_trace()
+    brain = catalog({'Type':'Profile', 'id':event.object.getId()})
+    if brain:
+        return
+    with api.env.adopt_user(user=currentUser):
+        with api.env.adopt_roles(['Manager']):
+            userFolder = catalog({'Type':'Folder', 'id':event.object.getId()})[0].getObject()
+#            api.content.transition(obj=userFolder, transition='publish')
+            userProfile = api.content.create(
+                container=userFolder,
+                type="tbfac.newcontent.profile",
+                id="index_html",
+                title=currentUser.getProperty("fullname")
+            )
+            userProfile.manage_permission('Copy or Move', roles=['Manager', 'Site Administrator'], acquire=False)
+            api.content.transition(obj=userProfile, transition='publish')
+
+    redirectUrl = userProfile.absolute_url()
+    response = event.object.REQUEST.response
+    response.redirect(redirectUrl, lock=True)
+    return
 
 
 @grok.subscribe(UserInitialLoginInEvent)
-def checkRoles(event):
+def initialAccount(event):
     portal = api.portal.get()
     currentUser = event.object
 #    import pdb; pdb.set_trace()
@@ -32,3 +61,4 @@ def checkRoles(event):
     response = event.object.REQUEST.response
     response.redirect(redirectUrl, lock=True)
     return
+
